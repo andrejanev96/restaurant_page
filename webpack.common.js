ef0c10e3e4ef972aad771b22995cb624a662a4e1
andrejanev96 <>
2025-05-20 20:05:35 +0200

    modified package.json to include scripts added webpack configuration files for common, development, and production environments

---
+++ webpack.common.js
@@ -0,0 +1,26 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: {
+    app: "./src/index.js",
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "Production",
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true,
+  },
+};
